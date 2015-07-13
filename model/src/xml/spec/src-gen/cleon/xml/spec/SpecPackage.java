bfd3ceb0eec4a78cbac1f51bce0328a2a20c0a0e
marcozwyssig <marco@zwyssig.info>
2015-07-14 01:01:35 +0200

    actifsource aktualisiert

---
+++ model/src/xml/spec/src-gen/cleon/xml/spec/SpecPackage.java
@@ -0,0 +1,26 @@
+package cleon.xml.spec;
+
+import ch.actifsource.core.Resource;
+
+public class SpecPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private SpecPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.xml.spec";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.xml.spec";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.xml.spec,34IYUH3bHYyVn9MAvpbFXFARY/8=] */

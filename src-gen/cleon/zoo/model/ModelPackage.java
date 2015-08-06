a921298b074619ae07d7650a03b54d4f90f41b12
marco.zwyssig <marco.zwyssig@achat.ekf.ugfu.admin.ch>
2015-08-06 09:38:06 +0200

    model verzeichnis aufgelöst

---
+++ src-gen/cleon/zoo/model/ModelPackage.java
@@ -0,0 +1,26 @@
+package cleon.zoo.model;
+
+import ch.actifsource.core.Resource;
+
+public class ModelPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private ModelPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.zoo.model";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.zoo.model";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.zoo.model,cO8qWWjum4cb0Wb/r7mpXOn9s6A=] */
---
+++ src-gen/cleon/zoo/model/specifications/SpecificationsPackage.java
@@ -0,0 +1,26 @@
+package cleon.zoo.model.specifications;
+
+import ch.actifsource.core.Resource;
+
+public class SpecificationsPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private SpecificationsPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.zoo.model";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.zoo.model.specifications";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.zoo.model.specifications,TVOSg4qEK34L6XRdJrACXsOpL44=] */

400b0c2d40d8d1aadaced844af268f8da7568f59
marcozwyssig <marco@zwyssig.info>
2015-09-05 16:25:13 +0200

    Markdown integriert

---
+++ src/devops/spec/src-gen/cleon/devops/spec/build/javamodel/IBuild.java
@@ -0,0 +1,11 @@
+package cleon.devops.spec.build.javamodel;
+
+import ch.actifsource.util.collection.IMultiMapOrdered;
+
+public interface IBuild extends cleon.devops.spec.javamodel.IBuildManagement {
+
+  public static final ch.actifsource.core.INode TYPE_ID = new ch.actifsource.core.Resource("cc39ac79-406e-11e5-b5f0-5b1ce9c3de3e");
+  
+}
+
+/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,cc39ac79-406e-11e5-b5f0-5b1ce9c3de3e,B7SXd03wy2j4NtCFYwF19dj2BHU=] */

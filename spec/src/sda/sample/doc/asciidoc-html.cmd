6954a8abcd3c9c30960ebdf25de511c7b6fba5b0
Marco Zwyssig <marco@zwyssig.info>
2016-01-25 14:59:39 +0100

    asciidoc automatic generate

---
+++ spec/src/sda/sample/doc/asciidoc-html.cmd
@@ -0,0 +1,5 @@
+cmd /c asciidoctor analysis/Sample_Analyse.adoc
+cmd /c asciidoctor Readme.adoc
+cmd /c asciidoctor project/Sample_Project.adoc
+cmd /c asciidoctor requirementsmanagement/Sample_Requirements_RequirementsManagement.adoc
+cmd /c asciidoctor project/scope/LO-1_Systemarchitektur/LO-1_Systemarchitektur_Backlog.adoc

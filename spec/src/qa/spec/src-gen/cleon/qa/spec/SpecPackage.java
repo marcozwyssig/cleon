f30350a659bd34292a5761e7bde23c0ba581a45f
Marco Zwyssig <marco@zwyssig.info>
2016-03-10 16:03:51 +0100

    .

--- spec/src/qa/spec/src-gen/cleon/qa/spec/SpecPackage.java
+++ spec/src/qa/spec/src-gen/cleon/qa/spec/SpecPackage.java
@@ -4,6 +4,8 @@ import ch.actifsource.core.Resource;
 
 public class SpecPackage {
   
+  public static final Resource QA = new Resource("30fcdfdb-e6d1-11e5-b492-dd1fef9f876f");
+  
   /* Begin Protected Region [[user]] */
   
   /* End Protected Region   [[user]] */
@@ -23,4 +25,4 @@ public class SpecPackage {
   }
   
 }
-/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.qa.spec,Fbj9+BJD9TJLcZM/dcME1XybB7g=] */
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.qa.spec,EO9bqtUVEoHE4WyuaXzy3wtXPEI=] */
---
+++ spec/src/qa/spec/src-gen/cleon/qa/spec/javamodel/IQA.java
@@ -0,0 +1,11 @@
+package cleon.qa.spec.javamodel;
+
+import ch.actifsource.util.collection.IMultiMapOrdered;
+
+public interface IQA extends ch.actifsource.core.javamodel.INamedResource {
+
+  public static final ch.actifsource.core.INode TYPE_ID = new ch.actifsource.core.Resource("30fcdfdb-e6d1-11e5-b492-dd1fef9f876f");
+  
+}
+
+/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,30fcdfdb-e6d1-11e5-b492-dd1fef9f876f,/dcjtq4COtQ6l9F2CFImEWHIjYw=] */

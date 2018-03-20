fcff367fae919e7eb863306474b50c72953c3c74
Marco Zwyssig <marco@zwyssig.info>
2018-03-20 11:17:24 +0100

    release 0.2.6

---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.conception.applications.spec.web/src-gen/cleon/conception/applications/spec/web/WebPackage.java
@@ -0,0 +1,26 @@
+package cleon.conception.applications.spec.web;
+
+import ch.actifsource.core.Resource;
+
+public class WebPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private WebPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.conception.applications.spec.web";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.conception.applications.spec.web";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.conception.applications.spec.web,vdaXoBiKDKrDe9CvaoVsKYB4bx0=] */
---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.support.documentation.template.asciidoc/src-gen/cleon/support/documentation/template/asciidoc/AsciidocPackage.java
@@ -0,0 +1,26 @@
+package cleon.support.documentation.template.asciidoc;
+
+import ch.actifsource.core.Resource;
+
+public class AsciidocPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private AsciidocPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.support.documentation.template.asciidoc";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.support.documentation.template.asciidoc";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.support.documentation.template.asciidoc,PXmQXwWndhmhYZnxZQaHahzwhjM=] */

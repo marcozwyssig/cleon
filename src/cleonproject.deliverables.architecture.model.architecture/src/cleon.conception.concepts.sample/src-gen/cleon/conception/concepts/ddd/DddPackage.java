4f6727a9a80b707fee32178165e044d8feeb9c07
Marco Zwyssig <marco@zwyssig.info>
2018-03-22 14:18:00 +0100

    refactored patterns und web

---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.conception.concepts.sample/src-gen/cleon/conception/concepts/ddd/DddPackage.java
@@ -0,0 +1,26 @@
+package cleon.conception.concepts.ddd;
+
+import ch.actifsource.core.Resource;
+
+public class DddPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private DddPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.conception.concepts.sample";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.conception.concepts.ddd";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.conception.concepts.ddd,3jaJ8EmLpDmbfzn+NpMoy7SEnNM=] */
---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.conception.concepts.spec.cqrs/src-gen/cleon/conception/cqrs/spec/cqrs/CqrsPackage.java
@@ -0,0 +1,28 @@
+package cleon.conception.cqrs.spec.cqrs;
+
+import ch.actifsource.core.Resource;
+
+public class CqrsPackage {
+  
+  public static final Resource CQRS = new Resource("ebb6e640-11f5-11e5-848b-017a3a98ae34");
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private CqrsPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.conception.concepts.spec.cqrs";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.conception.cqrs.spec.cqrs";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.conception.cqrs.spec.cqrs,dvEBOWD5meElleRlHMAXqZuk5h4=] */

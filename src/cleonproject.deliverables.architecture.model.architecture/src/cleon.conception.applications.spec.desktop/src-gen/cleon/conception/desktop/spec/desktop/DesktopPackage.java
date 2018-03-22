fa06338b1ae556e61ba204e5279a115f55a0877d
Marco Zwyssig <marco@zwyssig.info>
2018-03-22 13:34:30 +0100

    refactored applications features

---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.conception.applications.spec.desktop/src-gen/cleon/conception/desktop/spec/desktop/DesktopPackage.java
@@ -0,0 +1,28 @@
+package cleon.conception.desktop.spec.desktop;
+
+import ch.actifsource.core.Resource;
+
+public class DesktopPackage {
+  
+  public static final Resource Desktop = new Resource("05bff060-11f6-11e5-848b-017a3a98ae34");
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private DesktopPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.conception.applications.spec.desktop";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.conception.desktop.spec.desktop";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.conception.desktop.spec.desktop,GXF6NsEZyjHQTpTC0NgK4NFUFio=] */

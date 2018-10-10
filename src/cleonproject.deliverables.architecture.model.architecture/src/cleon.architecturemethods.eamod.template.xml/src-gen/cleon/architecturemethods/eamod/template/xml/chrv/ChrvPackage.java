bca78d4b6c85fb1c10e2d88bd15a79ccb23776c2
Marco Zwyssig <marco@zwyssig.info>
2018-10-10 20:08:06 +0200

    refactored packages in cleon.architecturemethods.eamod.template.xml

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.eamod.template.xml/src-gen/cleon/architecturemethods/eamod/template/xml/chrv/ChrvPackage.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.eamod.template.xml/src-gen/cleon/architecturemethods/eamod/template/xml/chrv/ChrvPackage.java
@@ -1,4 +1,4 @@
-package cleon.conception.architecture.template.eamod.xml.chrv;
+package cleon.architecturemethods.eamod.template.xml.chrv;
 
 import ch.actifsource.core.Resource;
 
@@ -19,8 +19,8 @@ public class ChrvPackage {
   }
   
   public static final String getPackageName() {
-    return "cleon.conception.architecture.template.eamod.xml.chrv";
+    return "cleon.architecturemethods.eamod.template.xml.chrv";
   }
   
 }
-/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.conception.architecture.template.eamod.xml.chrv,0Vr/JLVIjGr0pKFKMDFu6FZNp0g=] */
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.architecturemethods.eamod.template.xml.chrv,X6JD/v+PRlzdTRtFzeM4+fdFrTs=] */

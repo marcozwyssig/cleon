92760f9917d5bd9c2953afe8298d1816654f5c7a
Marco Zwyssig <marco@zwyssig.info>
2018-10-10 19:14:28 +0200

    refactored packages in cleon.architecturemethods.arc42.spec.microservices

---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.arc42.spec.microservices/src-gen/cleon/architecturemethods/arc42/spec/microservices/MicroservicesPackage.java
@@ -0,0 +1,26 @@
+package cleon.architecturemethods.arc42.spec.microservices;
+
+import ch.actifsource.core.Resource;
+
+public class MicroservicesPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private MicroservicesPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.architecturemethods.arc42.spec.microservices";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.architecturemethods.arc42.spec.microservices";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.architecturemethods.arc42.spec.microservices,hU2YWD/WMivXgRqeYnWME0w7WVI=] */

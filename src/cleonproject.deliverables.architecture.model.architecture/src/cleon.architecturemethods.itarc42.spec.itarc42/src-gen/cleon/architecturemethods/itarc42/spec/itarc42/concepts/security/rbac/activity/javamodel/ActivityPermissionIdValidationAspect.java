80080c0ebb19d0dd4f3dddb236e8aec1156e2216
Marco Zwyssig <marco@zwyssig.info>
2019-11-07 18:24:50 +0100

    id validating for activity and roles

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.itarc42.spec.itarc42/src-gen/cleon/architecturemethods/itarc42/spec/itarc42/concepts/security/rbac/activity/javamodel/ActivityPermissionIdValidationAspect.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.itarc42.spec.itarc42/src-gen/cleon/architecturemethods/itarc42/spec/itarc42/concepts/security/rbac/activity/javamodel/ActivityPermissionIdValidationAspect.java
@@ -1,5 +1,12 @@
 package cleon.architecturemethods.itarc42.spec.itarc42.concepts.security.rbac.activity.javamodel;
 
+import java.util.List;
+
+import ch.actifsource.core.dynamic.IDynamicResourceRepository;
+import ch.actifsource.core.validation.ValidationContext;
+import cleon.architecturemethods.itarc42.spec.itarc42.concepts.security.rbac.activity.FunctionSpace_Activity.IActivityPermissionFunctions;
+import cleon.architecturemethods.itarc42.spec.itarc42.concepts.security.rbac.role.FunctionSpace_Role.IRoleSystemComponentFunctions;
+import cleon.architecturemethods.itarc42.spec.itarc42.concepts.security.rbac.role.javamodel.IRoleSystemComponent;
 import cleon.common.resources.spec.resources.id.javamodel.BusinessObjectIdUniqueValidationAspect;
 
 public class ActivityPermissionIdValidationAspect extends BusinessObjectIdUniqueValidationAspect<IActivityPermission> 
@@ -8,4 +15,11 @@ public class ActivityPermissionIdValidationAspect extends BusinessObjectIdUnique
 	{
 		super(IActivityPermission.class);
 	}
+	
+	protected List<IActivityPermission> getResources(IDynamicResourceRepository resourceRepository, ValidationContext context) 
+	{
+		IActivityPermission component = resourceRepository.getResource(IActivityPermission.class, context.getResource());
+		IActivityPermissionFunctions functions = component.extension(IActivityPermissionFunctions.class);
+		return functions.AllActivityPermissionInDomain();
+	}	
 }
--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.itarc42.spec.itarc42/src-gen/cleon/architecturemethods/itarc42/spec/itarc42/concepts/security/rbac/role/javamodel/RoleSystemComponentIdValidationAspect.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.itarc42.spec.itarc42/src-gen/cleon/architecturemethods/itarc42/spec/itarc42/concepts/security/rbac/role/javamodel/RoleSystemComponentIdValidationAspect.java
@@ -1,5 +1,10 @@
 package cleon.architecturemethods.itarc42.spec.itarc42.concepts.security.rbac.role.javamodel;
 
+import java.util.List;
+
+import ch.actifsource.core.dynamic.IDynamicResourceRepository;
+import ch.actifsource.core.validation.ValidationContext;
+import cleon.architecturemethods.itarc42.spec.itarc42.concepts.security.rbac.role.FunctionSpace_Role.IRoleSystemComponentFunctions;
 import cleon.common.resources.spec.resources.id.javamodel.BusinessObjectIdUniqueValidationAspect;
 
 public class RoleSystemComponentIdValidationAspect extends BusinessObjectIdUniqueValidationAspect<IRoleSystemComponent> {
@@ -7,4 +12,12 @@ public class RoleSystemComponentIdValidationAspect extends BusinessObjectIdUniqu
 	{
 		super(IRoleSystemComponent.class);
 	}
+	
+	protected List<IRoleSystemComponent> getResources(IDynamicResourceRepository resourceRepository, ValidationContext context) 
+	{
+		IRoleSystemComponent component = resourceRepository.getResource(IRoleSystemComponent.class, context.getResource());
+		IRoleSystemComponentFunctions functions = component.extension(IRoleSystemComponentFunctions.class);
+		return functions.AllRoleSystemComponentInDomain();
+	}
+		
 }

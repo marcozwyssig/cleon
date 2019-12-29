f34b56949fc3bf3bf495bf85f7111ede7fcbd323
Marco Zwyssig <marco@zwyssig.info>
2019-12-29 21:10:27 +0100

    added refactoring for disable root

---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.common.actifsource.metamodel/src-gen/cleon/common/actifsource/metamodel/refactoring/RefactorDisableRoot.java
@@ -0,0 +1,42 @@
+package cleon.common.actifsource.metamodel.refactoring;
+
+import java.util.Date;
+
+import ch.actifsource.core.CorePackage;
+import ch.actifsource.core.INode;
+import ch.actifsource.core.Package;
+import ch.actifsource.core.Relationship;
+import ch.actifsource.core.Resource;
+import ch.actifsource.core.javamodel.IStatement;
+import ch.actifsource.core.javamodel.Statement;
+import ch.actifsource.core.job.Select;
+import ch.actifsource.core.job.Update;
+import ch.actifsource.core.model.aspects.impl.AbstractAllInstancesRefactorerAspect;
+import ch.actifsource.core.patch.IStatementPosition;
+import ch.actifsource.core.set.INodeList;
+import ch.actifsource.core.set.INodeSet;
+import ch.actifsource.core.set.IPackageSet;
+import ch.actifsource.core.set.IStatementSet;
+import ch.actifsource.core.update.FixOwnership;
+import ch.actifsource.core.update.IModifiable;
+import ch.actifsource.core.util.LiteralUtil;
+import ch.actifsource.util.log.Logger;
+
+public class RefactorDisableRoot extends AbstractAllInstancesRefactorerAspect {
+	
+	public RefactorDisableRoot() {
+		super("1.0", 2019, 11, 28, "Disable root resource", CorePackage.Class);
+	}
+
+	@Override
+	protected void refactor(IModifiable executor, Package _package, INode clazz) {	
+		INode value = Select.objectForAttributeOrNull(executor, CorePackage.Class_allowRoot, clazz);
+		if( value != null ) {
+			boolean boolValue = LiteralUtil.getBooleanValue(value);
+			if(boolValue == false) {
+				return;
+			}
+		}
+		Update.createOrModifyStatement(executor, _package, clazz, CorePackage.Class_allowRoot, LiteralUtil.create(false));
+	}
+}
---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.common.actifsource.metamodel/src-gen/cleon/common/actifsource/metamodel/refactoring/RefactoringPackage.java
@@ -0,0 +1,26 @@
+package cleon.common.actifsource.metamodel.refactoring;
+
+import ch.actifsource.core.Resource;
+
+public class RefactoringPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private RefactoringPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.common.actifsource.metamodel";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.common.actifsource.metamodel.refactoring";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.common.actifsource.metamodel.refactoring,tQRmJS7apt6jF3ehwOd74iEuS2Q=] */

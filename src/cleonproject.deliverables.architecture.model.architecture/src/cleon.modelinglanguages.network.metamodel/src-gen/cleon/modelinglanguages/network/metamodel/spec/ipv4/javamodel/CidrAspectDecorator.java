3cb1b880dfb348abdecb07bb6ed0250c2be7d6da
Marco Zwyssig <marco@zwyssig.info>
2020-05-01 10:28:42 +0200

    supporting /32 network

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.modelinglanguages.network.metamodel/src-gen/cleon/modelinglanguages/network/metamodel/spec/ipv4/javamodel/CidrAspectDecorator.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.modelinglanguages.network.metamodel/src-gen/cleon/modelinglanguages/network/metamodel/spec/ipv4/javamodel/CidrAspectDecorator.java
@@ -11,6 +11,7 @@ import ch.actifsource.core.selector.typesystem.ITypeSystem;
 import ch.actifsource.core.selector.typesystem.impl.TypeSystem;
 import ch.actifsource.core.set.INodeSet;
 import ch.actifsource.core.set.NodeSet;
+import ch.actifsource.util.log.Logger;
 import cleon.modelinglanguages.network.metamodel.spec.ipv4.FunctionSpace_IP.IIPRangeFunctions;
 import cleon.modelinglanguages.network.metamodel.spec.ipv4.FunctionSpace_IP.IIPv4_MaskFunctions;
 import cleon.modelinglanguages.network.metamodel.spec.javamodel.IAbstractNetwork;
@@ -35,14 +36,26 @@ public class CidrAspectDecorator extends AspectImplementationDecorator {
 			return;
 		}
 		
+		if (cidr.selectMask() <= 20 && cidr.selectMask() > 32) {
+			return;
+		}
+		
 		try {
 			SubnetUtils subnet = new SubnetUtils(Select.simpleName(executor, cidr.getResource()));
-			for( String ip : subnet.getInfo().getAllAddresses())
-			{
-				IIPv4_D ipv4 = range.extension(IIPRangeFunctions.class).toIPv4(ip);
+		
+			if (cidr.selectMask() == 32 ) {
+				IIPv4_D ipv4 = range.extension(IIPRangeFunctions.class).toIPv4(subnet.getInfo().getAddress());				
 				if( ipv4 != null) {
 					ipNodeSet.add(ipv4.getResource());
-				}
+				}				
+			} else {
+				for( String ip : subnet.getInfo().getAllAddresses())
+				{
+					IIPv4_D ipv4 = range.extension(IIPRangeFunctions.class).toIPv4(ip);
+					if( ipv4 != null) {
+						ipNodeSet.add(ipv4.getResource());
+					}
+				}				
 			}
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();

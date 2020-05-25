bdf19828b87f5831e170cb3a04ada3c54c5781c7
Marco Zwyssig <marco@zwyssig.info>
2020-05-25 18:08:27 +0200

    fixed some issues

---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/template/txt/HostGroups_aE_All__T_csv.java
@@ -0,0 +1,48 @@
+package cleon.architecturemethods.systemarc42.metamodel.template.txt;
+
+import ch.actifsource.util.Assert;
+import java.util.List;
+import ch.actifsource.core.dynamic.DynamicResourceUtil;
+import ch.actifsource.core.dynamic.IDynamicResourceExtension;
+import ch.actifsource.core.dynamic.IDynamicResourceExtensionJavaImpl;
+import ch.actifsource.core.selector.typesystem.JavaFunctionUtil;
+
+/* Begin Protected Region [[6c647615-9e8a-11ea-9f3e-ed1c2e923327,imports]] */
+
+/* End Protected Region   [[6c647615-9e8a-11ea-9f3e-ed1c2e923327,imports]] */
+
+public class HostGroups_aE_All__T_csv {
+
+  /* Begin Protected Region [[6c647615-9e8a-11ea-9f3e-ed1c2e923327]] */
+  
+  /* End Protected Region   [[6c647615-9e8a-11ea-9f3e-ed1c2e923327]] */
+
+
+  public static interface IAbstractSiteWithHostsFunctions extends IDynamicResourceExtension {
+
+    @IDynamicResourceExtension.MethodId("6c64762b-9e8a-11ea-9f3e-ed1c2e923327")
+    public java.lang.String GroupName(final cleon.modelinglanguages.segmentation.metamodel.spec.javamodel.ISecuritySubZone securitySubZone, final cleon.architecturemethods.systemarc42.metamodel.spec._05_buildingblock_view.javamodel.ISystemConfiguration systemConfiguration);
+
+  }
+  
+  public static interface IAbstractSiteWithHostsFunctionsImpl extends IDynamicResourceExtensionJavaImpl {
+    
+  }
+  
+  public static class AbstractSiteWithHostsFunctionsImpl implements IAbstractSiteWithHostsFunctionsImpl {
+
+    public static final IAbstractSiteWithHostsFunctionsImpl INSTANCE = new AbstractSiteWithHostsFunctionsImpl();
+
+    private AbstractSiteWithHostsFunctionsImpl() {}
+
+  }
+  
+  public static class AbstractSiteWithHostsFunctions {
+
+    private AbstractSiteWithHostsFunctions() {}
+
+  }
+
+}
+
+/* Actifsource ID=[5349246f-db37-11de-82b8-17be2e034a3b,6c647615-9e8a-11ea-9f3e-ed1c2e923327,ghOi6PJgLMrc4x1hT1wG2yRaF9I=] */
--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/template/txt/TxtPackage.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/template/txt/TxtPackage.java
@@ -4,6 +4,8 @@ import ch.actifsource.core.Resource;
 
 public class TxtPackage {
   
+  public static final Resource HostGroups_aE_All__T_csv_AbstractSiteWithHosts = new Resource("6c64762c-9e8a-11ea-9f3e-ed1c2e923327");
+  
   /* Begin Protected Region [[user]] */
   
   /* End Protected Region   [[user]] */
@@ -23,4 +25,4 @@ public class TxtPackage {
   }
   
 }
-/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.architecturemethods.systemarc42.metamodel.template.txt,i0cOUOmvJznR5eWI5vnGsHvXYdA=] */
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.architecturemethods.systemarc42.metamodel.template.txt,3srUTCtzbGrRmhg2IzCbF+OwuU4=] */

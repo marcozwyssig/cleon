48adaa83e8f883f464e9c002e65ce56545959c25
Marco Zwyssig <marco@zwyssig.info>
2020-06-08 20:40:31 +0200

    csv import wizard

---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/importer/csv/CsvPackage.java
@@ -0,0 +1,26 @@
+package cleon.architecturemethods.systemarc42.metamodel.importer.csv;
+
+import ch.actifsource.core.Resource;
+
+public class CsvPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private CsvPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.architecturemethods.systemarc42.metamodel";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.architecturemethods.systemarc42.metamodel.importer.csv";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.architecturemethods.systemarc42.metamodel.importer.csv,Pb7eswChQp1jdvSMc2JDxckBG3c=] */

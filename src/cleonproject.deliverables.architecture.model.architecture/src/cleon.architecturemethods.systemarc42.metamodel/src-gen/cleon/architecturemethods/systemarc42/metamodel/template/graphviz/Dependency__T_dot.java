563996e8cf5a5ba0a7b83dd624fa54ed595b46d9
Marco Zwyssig <marco@zwyssig.info>
2020-07-10 09:04:55 +0200

    made svg unique

---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/template/graphviz/Dependency__T_dot.java
@@ -0,0 +1,48 @@
+package cleon.architecturemethods.systemarc42.metamodel.template.graphviz;
+
+import ch.actifsource.util.Assert;
+import java.util.List;
+import ch.actifsource.core.dynamic.DynamicResourceUtil;
+import ch.actifsource.core.dynamic.IDynamicResourceExtension;
+import ch.actifsource.core.dynamic.IDynamicResourceExtensionJavaImpl;
+import ch.actifsource.core.selector.typesystem.JavaFunctionUtil;
+
+/* Begin Protected Region [[93a1c78e-c10d-11ea-be61-6597e1b9dc2b,imports]] */
+
+/* End Protected Region   [[93a1c78e-c10d-11ea-be61-6597e1b9dc2b,imports]] */
+
+public class Dependency__T_dot {
+
+  /* Begin Protected Region [[93a1c78e-c10d-11ea-be61-6597e1b9dc2b]] */
+  
+  /* End Protected Region   [[93a1c78e-c10d-11ea-be61-6597e1b9dc2b]] */
+
+
+  public static interface IBuildingBlockFunctions extends IDynamicResourceExtension {
+
+    @IDynamicResourceExtension.MethodId("1b51efb0-c278-11ea-b879-e9e2b2ad65df")
+    public java.lang.String UniqueName();
+
+  }
+  
+  public static interface IBuildingBlockFunctionsImpl extends IDynamicResourceExtensionJavaImpl {
+    
+  }
+  
+  public static class BuildingBlockFunctionsImpl implements IBuildingBlockFunctionsImpl {
+
+    public static final IBuildingBlockFunctionsImpl INSTANCE = new BuildingBlockFunctionsImpl();
+
+    private BuildingBlockFunctionsImpl() {}
+
+  }
+  
+  public static class BuildingBlockFunctions {
+
+    private BuildingBlockFunctions() {}
+
+  }
+
+}
+
+/* Actifsource ID=[5349246f-db37-11de-82b8-17be2e034a3b,93a1c78e-c10d-11ea-be61-6597e1b9dc2b,XIABuDgjH1gwXBJE/reLKBlYcT4=] */
--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/template/graphviz/GraphvizPackage.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/template/graphviz/GraphvizPackage.java
@@ -7,6 +7,7 @@ public class GraphvizPackage {
   public static final Resource DeploymentDiagram__T_dot_Node = new Resource("cc21754e-853f-11ea-9f0c-755186b47bc2");
   public static final Resource System_aE_Architecture_aE_Overview__T_dot_BuildingBlock = new Resource("69ef5324-baaa-11ea-b29c-1fc1209c2c7a");
   public static final Resource System_aE_Architecture_aE_Overview_aE_2__T_dot_ComponentComposition = new Resource("eb42de3f-c100-11ea-bf72-ff157146cf2f");
+  public static final Resource Dependency__T_dot_BuildingBlock = new Resource("1b50690c-c278-11ea-b879-e9e2b2ad65df");
   
   /* Begin Protected Region [[user]] */
   
@@ -27,4 +28,4 @@ public class GraphvizPackage {
   }
   
 }
-/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.architecturemethods.systemarc42.metamodel.template.graphviz,+aX4KWXNhq6w3Sq/b9vkSKlVdVk=] */
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.architecturemethods.systemarc42.metamodel.template.graphviz,/VpMPmAu6bUuzWAXW+fkDtOcx4Y=] */

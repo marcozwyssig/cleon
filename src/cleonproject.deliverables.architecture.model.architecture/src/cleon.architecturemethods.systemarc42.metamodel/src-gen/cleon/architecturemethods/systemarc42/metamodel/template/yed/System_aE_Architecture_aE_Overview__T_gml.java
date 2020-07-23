3345640dc8697d0a317fd222d6e9a711ca9f1e8b
Marco Zwyssig <marco@zwyssig.info>
2020-07-23 07:27:45 +0200

    added yed template

---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/template/yed/System_aE_Architecture_aE_Overview__T_gml.java
@@ -0,0 +1,81 @@
+package cleon.architecturemethods.systemarc42.metamodel.template.yed;
+
+import ch.actifsource.core.dynamic.DynamicResourceUtil;
+import ch.actifsource.core.dynamic.IDynamicResourceExtension;
+import ch.actifsource.core.dynamic.IDynamicResourceExtensionJavaImpl;
+
+/* Begin Protected Region [[21062948-cc3b-11ea-9aba-81170876b0e5,imports]] */
+
+/* End Protected Region   [[21062948-cc3b-11ea-9aba-81170876b0e5,imports]] */
+
+public class System_aE_Architecture_aE_Overview__T_gml {
+
+	/* Begin Protected Region [[21062948-cc3b-11ea-9aba-81170876b0e5]] */
+
+	/* End Protected Region   [[21062948-cc3b-11ea-9aba-81170876b0e5]] */
+
+
+	public interface IComponentCompositionFunctions extends IDynamicResourceExtension {
+
+		@IDynamicResourceExtension.MethodId("210650bb-cc3b-11ea-9aba-81170876b0e5") java.lang.String GenerateComponents();
+
+	}
+
+	public interface IComponentCompositionFunctionsImpl extends IDynamicResourceExtensionJavaImpl {
+
+	}
+
+	public static class ComponentCompositionFunctionsImpl implements IComponentCompositionFunctionsImpl {
+
+		public static final IComponentCompositionFunctionsImpl INSTANCE = new ComponentCompositionFunctionsImpl();
+
+		private ComponentCompositionFunctionsImpl() {}
+
+	}
+
+	public static class ComponentCompositionFunctions {
+
+		private ComponentCompositionFunctions() {}
+
+	}
+
+	public interface IGuidFunctions extends IDynamicResourceExtension {
+
+		@IDynamicResourceExtension.MethodId("ced062d0-cc4e-11ea-8b73-6b1c6b53a7a6") java.lang.Integer id();
+
+	}
+
+	public interface IGuidFunctionsImpl extends IDynamicResourceExtensionJavaImpl {
+
+		@IDynamicResourceExtension.MethodId("ced062d0-cc4e-11ea-8b73-6b1c6b53a7a6") java.lang.Integer id(final java.util.UUID guid);
+
+	}
+
+	public static class GuidFunctionsImpl implements IGuidFunctionsImpl {
+
+		public static final IGuidFunctionsImpl INSTANCE = new GuidFunctionsImpl();
+
+		private GuidFunctionsImpl() {}
+
+		@Override
+		public java.lang.Integer id(final java.util.UUID guid) {
+			/* Begin Protected Region [[ced062d0-cc4e-11ea-8b73-6b1c6b53a7a6]] */
+			return guid.hashCode();
+			/* End Protected Region   [[ced062d0-cc4e-11ea-8b73-6b1c6b53a7a6]] */
+		}
+
+	}
+
+	public static class GuidFunctions {
+
+		private GuidFunctions() {}
+
+		public static java.lang.Integer id(final java.util.UUID guid) {
+			return DynamicResourceUtil.invoke(IGuidFunctionsImpl.class, GuidFunctionsImpl.INSTANCE, guid).id(guid);
+		}
+
+	}
+
+}
+
+/* Actifsource ID=[5349246f-db37-11de-82b8-17be2e034a3b,21062948-cc3b-11ea-9aba-81170876b0e5,NhLd5993pdy2lmAhKr1jXJQJfao=] */
---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/template/yed/YedPackage.java
@@ -0,0 +1,29 @@
+package cleon.architecturemethods.systemarc42.metamodel.template.yed;
+
+import ch.actifsource.core.Resource;
+
+public class YedPackage {
+  
+  public static final Resource System_aE_Architecture_aE_Overview__T_gml_ComponentComposition = new Resource("210650bc-cc3b-11ea-9aba-81170876b0e5");
+  public static final Resource System_aE_Architecture_aE_Overview__T_gml_Guid = new Resource("ced03bbc-cc4e-11ea-8b73-6b1c6b53a7a6");
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private YedPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.architecturemethods.systemarc42.metamodel";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.architecturemethods.systemarc42.metamodel.template.yed";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.architecturemethods.systemarc42.metamodel.template.yed,jcGiDPEUFWyzdeelnQMpnEfgpTI=] */

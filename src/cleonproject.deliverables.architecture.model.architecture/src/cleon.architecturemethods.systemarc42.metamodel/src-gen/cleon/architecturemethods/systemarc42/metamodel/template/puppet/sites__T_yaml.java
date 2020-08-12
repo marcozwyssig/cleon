6fc8f999426e424bdade1551b3b5e444748a462b
Marco Zwyssig <marco@zwyssig.info>
2020-08-12 19:43:45 +0200

    improved sites generation

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/template/puppet/sites__T_yaml.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/template/puppet/sites__T_yaml.java
@@ -10,8 +10,11 @@ import ch.actifsource.core.selector.typesystem.JavaFunctionUtil;
 /* Begin Protected Region [[5a5e3d83-da22-11ea-ae00-5518e944c256,imports]] */
 import cleon.architecturemethods.arc42.metamodel.spec._07_deployment_view.monitor.buildingblocks.javamodel.MonitoringBuildingBlock;
 import cleon.architecturemethods.systemarc42.metamodel.spec._05_buildingblock_view.systemconfiguration.javamodel.IPrtgProbeSystemConfiguration;
+import cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractHost;
 import ch.actifsource.core.job.Select;
 import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
 import ch.actifsource.environment.EnvironmentPlugin;
 /* End Protected Region   [[5a5e3d83-da22-11ea-ae00-5518e944c256,imports]] */
 
@@ -33,6 +36,12 @@ public class sites__T_yaml {
     @IDynamicResourceExtension.MethodId("318d7935-da2d-11ea-ae00-5518e944c256")
     public java.lang.String GetProbeName(final cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractSite site);
 
+    @IDynamicResourceExtension.MethodId("c0109767-dcb7-11ea-b5f8-77c3980a1d0a")
+    public java.lang.String RenderToText(final cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractSite site);
+
+    @IDynamicResourceExtension.MethodId("012a2be1-dcba-11ea-b5f8-77c3980a1d0a")
+    public java.lang.String Entry();
+
   }
   
   public static interface IAbstractHostFunctionsImpl extends IDynamicResourceExtensionJavaImpl {
@@ -42,6 +51,9 @@ public class sites__T_yaml {
 
     @IDynamicResourceExtension.MethodId("318d7935-da2d-11ea-ae00-5518e944c256")
     public java.lang.String GetProbeName(final cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractSite site, final cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractHost abstractHost);
+
+    @IDynamicResourceExtension.MethodId("c0109767-dcb7-11ea-b5f8-77c3980a1d0a")
+    public java.lang.String RenderToText(final cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractSite site, final List<cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractHost> abstractHostList);
 
   }
   
@@ -82,6 +94,44 @@ public class sites__T_yaml {
       /* End Protected Region   [[318d7935-da2d-11ea-ae00-5518e944c256]] */
     }
 
+    @Override
+    public java.lang.String RenderToText(final cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractSite site, final List<cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractHost> abstractHostList) {
+      /* Begin Protected Region [[c0109767-dcb7-11ea-b5f8-77c3980a1d0a]] */
+      final HashMap<String, HashMap<String, List<String>>> hashtable = new HashMap<>();
+      for( final IAbstractHost abstractHost : abstractHostList) { 
+      	final IAbstractHostFunctions abstractHostFunctions = abstractHost.extension(IAbstractHostFunctions.class);
+      	final String probeName =  abstractHostFunctions.GetProbeName(site);
+      	if (!hashtable.containsKey(probeName)) {
+      		hashtable.put(probeName, new HashMap<>());
+      	}
+      	final String pathName = abstractHostFunctions.GetPath();
+      	if(!hashtable.get(probeName).containsKey(pathName)) {
+      		hashtable.get(probeName).put(pathName, new ArrayList<>());
+      	}
+      	hashtable.get(probeName).get(pathName).add(abstractHostFunctions.Entry());
+      }
+      final String siteName = Select.simpleName(EnvironmentPlugin.getGlobalReadJobExecutor(), site.getResource()); 
+
+      final StringBuffer stringBuffer = new StringBuffer();
+      for( final String probe : hashtable.keySet() ) {
+      	final HashMap<String, List<String>> pathTable = hashtable.get(probe);
+      	for( final String path : pathTable.keySet()) {
+      		stringBuffer.append(String.format("  %s|%s|%s\n", probe, siteName, path));
+      		stringBuffer.append("    Profile: pf_grp_ikt\n");
+      		stringBuffer.append("    Devices:\n");
+      		for( final String entry : pathTable.get(path)) {
+      			stringBuffer.append(String.format("      %s\n",entry));						
+      		}
+      		stringBuffer.append("\n");
+      	}
+      }
+      if (stringBuffer.length() == 0) {
+      	return null;
+      }
+      return stringBuffer.toString(); 
+      /* End Protected Region   [[c0109767-dcb7-11ea-b5f8-77c3980a1d0a]] */
+    }
+
   }
   
   public static class AbstractHostFunctions {
@@ -94,6 +144,10 @@ public class sites__T_yaml {
 
     public static java.lang.String GetProbeName(final cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractSite site, final cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractHost abstractHost) {
       return DynamicResourceUtil.invoke(IAbstractHostFunctionsImpl.class, AbstractHostFunctionsImpl.INSTANCE, abstractHost).GetProbeName(site, abstractHost);
+    }
+
+    public static java.lang.String RenderToText(final cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractSite site, final List<cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractHost> abstractHostList) {
+      return DynamicResourceUtil.invoke(IAbstractHostFunctionsImpl.class, AbstractHostFunctionsImpl.INSTANCE, abstractHostList).RenderToText(site, abstractHostList);
     }
 
   }
@@ -125,4 +179,4 @@ public class sites__T_yaml {
 
 }
 
-/* Actifsource ID=[5349246f-db37-11de-82b8-17be2e034a3b,5a5e3d83-da22-11ea-ae00-5518e944c256,7jDPnEvYkYM1B8ibfFE3daHahxM=] */
+/* Actifsource ID=[5349246f-db37-11de-82b8-17be2e034a3b,5a5e3d83-da22-11ea-ae00-5518e944c256,ZA8TX7PJ2dgNSIH9Krc8QYpxUWk=] */

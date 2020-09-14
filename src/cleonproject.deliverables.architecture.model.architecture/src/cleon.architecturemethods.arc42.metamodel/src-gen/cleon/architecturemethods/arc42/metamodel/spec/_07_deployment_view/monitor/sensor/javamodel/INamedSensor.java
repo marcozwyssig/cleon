e6cadf239111b9da23949fc18bf0dfda64e68425
Marco Zwyssig <marco@zwyssig.info>
2020-09-14 18:05:39 +0200

    changed order

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.arc42.metamodel/src-gen/cleon/architecturemethods/arc42/metamodel/spec/_07_deployment_view/monitor/sensor/javamodel/INamedSensor.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.arc42.metamodel/src-gen/cleon/architecturemethods/arc42/metamodel/spec/_07_deployment_view/monitor/sensor/javamodel/INamedSensor.java
@@ -2,10 +2,10 @@ package cleon.architecturemethods.arc42.metamodel.spec._07_deployment_view.monit
 
 import ch.actifsource.util.collection.IMultiMapOrdered;
 
-public interface INamedSensor extends cleon.architecturemethods.arc42.metamodel.spec._07_deployment_view.monitor.sensor.javamodel.ISensor, ch.actifsource.core.javamodel.INamedResource {
+public interface INamedSensor extends ch.actifsource.core.javamodel.INamedResource, cleon.architecturemethods.arc42.metamodel.spec._07_deployment_view.monitor.sensor.javamodel.ISensor {
 
   public static final ch.actifsource.core.INode TYPE_ID = new ch.actifsource.core.Resource("23f80945-f3fb-11ea-ade2-eb32c9704a85");
   
 }
 
-/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,23f80945-f3fb-11ea-ade2-eb32c9704a85,IrQMnhRf7quk5r/38KLyCV83x9g=] */
+/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,23f80945-f3fb-11ea-ade2-eb32c9704a85,MZGzi5HNkDYsduYEc1FBxJ/C47U=] */

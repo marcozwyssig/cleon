79430b511e5c6e0de9ad679ace4a9f3304eeabdd
Marco Zwyssig <marco@zwyssig.info>
2020-11-25 11:57:26 +0100

    fixes

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.arc42.metamodel/src-gen/cleon/architecturemethods/arc42/metamodel/spec/_05_buildingblock_view/javamodel/Backup_and_Recovery.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.arc42.metamodel/src-gen/cleon/architecturemethods/arc42/metamodel/spec/_05_buildingblock_view/javamodel/Backup_and_Recovery.java
@@ -62,6 +62,16 @@ public class Backup_and_Recovery extends DynamicResource implements IBackup_and_
   }
     
   @Override
+  public cleon.common.doc.metamodel.spec.chapter.javamodel.IAbstractChapter selectNoChapters() {
+    return _getSingle(cleon.common.doc.metamodel.spec.chapter.javamodel.IAbstractChapter.class, cleon.common.doc.metamodel.spec.chapter.ChapterPackage.NoChapters_noChapters);
+  }
+
+  public Backup_and_Recovery setNoChapters(cleon.common.doc.metamodel.spec.chapter.javamodel.IAbstractChapter noChapters) {
+    _setSingle(cleon.common.doc.metamodel.spec.chapter.ChapterPackage.NoChapters_noChapters, noChapters);
+    return this;
+  }
+    
+  @Override
   public java.util.List<? extends cleon.common.doc.metamodel.spec.paragraph.javamodel.IParagraph> selectParagraphs() {
     return _getList(cleon.common.doc.metamodel.spec.paragraph.javamodel.IParagraph.class, cleon.common.doc.metamodel.spec.chapter.ChapterPackage.ParagraphAware_paragraphs);
   }
@@ -107,7 +117,6 @@ public class Backup_and_Recovery extends DynamicResource implements IBackup_and_
     // attributes
     _acceptListAttribute(java.lang.String.class, cleon.common.resources.metamodel.spec.descriptions.DescriptionsPackage.SimpleDescription_descriptions, visitor);
     // relations
-    _acceptList(cleon.common.doc.metamodel.spec.chapter.javamodel.IAbstractChapter.class, cleon.common.doc.metamodel.spec.SpecPackage.ChapterAware_chapters, visitor);
     _acceptList(cleon.common.doc.metamodel.spec.paragraph.javamodel.IParagraph.class, cleon.common.doc.metamodel.spec.chapter.ChapterPackage.ParagraphAware_paragraphs, visitor);
     _acceptSingle(ch.actifsource.core.javamodel.IClass.class, ch.actifsource.core.CorePackage.Resource_typeOf, visitor);
     _acceptSingle(cleon.architecturemethods.arc42.metamodel.spec._08_concepts.system.businesscontinuity.backup.javamodel.IBackupBuildingBlock.class, cleon.architecturemethods.arc42.metamodel.spec._05_buildingblock_view._05_buildingblock_viewPackage.Backup_aE_and_aE_Recovery_usedBackupBuildingBlock, visitor);
@@ -120,4 +129,4 @@ public class Backup_and_Recovery extends DynamicResource implements IBackup_and_
   }
   
 }
-/* Actifsource ID=[4d723cb5-db37-11de-82b8-17be2e034a3b,0bc5739a-7ed3-11ea-9262-7b7e0e9cb204,RJjdOdDXPGEYgz7y2FBgxTUwlIw=] */
+/* Actifsource ID=[4d723cb5-db37-11de-82b8-17be2e034a3b,0bc5739a-7ed3-11ea-9262-7b7e0e9cb204,gjOTTXOl35O9IqpKYnAfJv6FYe0=] */
--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.arc42.metamodel/src-gen/cleon/architecturemethods/arc42/metamodel/spec/_05_buildingblock_view/javamodel/IBackup_and_Recovery.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.arc42.metamodel/src-gen/cleon/architecturemethods/arc42/metamodel/spec/_05_buildingblock_view/javamodel/IBackup_and_Recovery.java
@@ -2,7 +2,7 @@ package cleon.architecturemethods.arc42.metamodel.spec._05_buildingblock_view.ja
 
 import ch.actifsource.util.collection.IMultiMapOrdered;
 
-public interface IBackup_and_Recovery extends cleon.common.doc.metamodel.spec.chapter.javamodel.IAbstractChapter, ch.actifsource.core.javamodel.IDecorator {
+public interface IBackup_and_Recovery extends cleon.common.doc.metamodel.spec.chapter.javamodel.IAbstractChapter, ch.actifsource.core.javamodel.IDecorator, cleon.common.doc.metamodel.spec.chapter.javamodel.INoChapters {
 
   public static final ch.actifsource.core.INode TYPE_ID = new ch.actifsource.core.Resource("0bc5739a-7ed3-11ea-9262-7b7e0e9cb204");
   
@@ -12,4 +12,4 @@ public interface IBackup_and_Recovery extends cleon.common.doc.metamodel.spec.ch
   
 }
 
-/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,0bc5739a-7ed3-11ea-9262-7b7e0e9cb204,p688ouj0t0lJdhMLel3Cql2aoT0=] */
+/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,0bc5739a-7ed3-11ea-9262-7b7e0e9cb204,i/rIeftWSWmbhTXJC0hmjh3hVFo=] */

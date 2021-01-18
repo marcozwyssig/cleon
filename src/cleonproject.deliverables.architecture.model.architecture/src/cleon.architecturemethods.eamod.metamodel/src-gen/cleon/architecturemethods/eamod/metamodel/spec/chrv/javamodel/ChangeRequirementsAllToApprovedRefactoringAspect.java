cb175c361f3756bc8fbb5e277f342f6ceef217fa
Marco Zwyssig <marco@zwyssig.info>
2021-01-18 14:49:23 +0100

    added xml for requirements

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.eamod.metamodel/src-gen/cleon/architecturemethods/eamod/metamodel/spec/chrv/javamodel/ChangeRequirementsAllToApprovedRefactoringAspect.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.eamod.metamodel/src-gen/cleon/architecturemethods/eamod/metamodel/spec/chrv/javamodel/ChangeRequirementsAllToApprovedRefactoringAspect.java
@@ -2,10 +2,8 @@ package cleon.architecturemethods.eamod.metamodel.spec.chrv.javamodel;
 
 import ch.actifsource.core.INode;
 import ch.actifsource.core.Package;
-import ch.actifsource.core.dynamic.IDynamicResourceRepository;
 import ch.actifsource.core.job.Update;
 import ch.actifsource.core.model.aspects.impl.AbstractAllInstancesRefactorerAspect;
-import ch.actifsource.core.selector.typesystem.ITypeSystem;
 import ch.actifsource.core.selector.typesystem.impl.TypeSystem;
 import ch.actifsource.core.update.IModifiable;
 import cleon.architecturemethods.eamod.metamodel.spec.chrv.requirements.subjectareas.requirement.RequirementPackage;
@@ -19,14 +17,14 @@ public class ChangeRequirementsAllToApprovedRefactoringAspect extends AbstractAl
 
 	@Override
 	protected void refactor(IModifiable executor, Package paramPackage, INode paramINode) {
-		ITypeSystem typeSystem = TypeSystem.create(executor);
-		IDynamicResourceRepository resourceRepository = typeSystem.getResourceRepository();
+		final var typeSystem = TypeSystem.create(executor);
+		final var resourceRepository = typeSystem.getResourceRepository();
 
-		IRequirement requirement = resourceRepository.getResource(IRequirement.class, paramINode);
+		final var requirement = resourceRepository.getResource(IRequirement.class, paramINode);
 
-		if (requirement.selectState().isAligned()) {
-			Update.createOrModifyStatement(executor, paramPackage, paramINode,
-					RequirementPackage.Requirement_state, RequirementPackage.RequirementState_Approved);
+		if (requirement.selectState().isAligned() || requirement.selectState().isApproved__F___N_By__F_Supplier__O_()) {
+			Update.createOrModifyStatement(executor, paramPackage, paramINode, RequirementPackage.Requirement_state,
+					RequirementPackage.RequirementState_Approved);
 		}
 	}
 }

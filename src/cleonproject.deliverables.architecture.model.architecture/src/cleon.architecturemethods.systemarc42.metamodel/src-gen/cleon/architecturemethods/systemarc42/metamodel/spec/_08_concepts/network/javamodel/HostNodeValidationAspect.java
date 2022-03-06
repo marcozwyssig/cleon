fe16468248e53b8d4e811b98c5f6daeb95a8308b
Marco Zwyssig <marco@zwyssig.info>
2022-03-06 13:03:00 +0100

    added quickfix

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/spec/_08_concepts/network/javamodel/HostNodeValidationAspect.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/spec/_08_concepts/network/javamodel/HostNodeValidationAspect.java
@@ -2,36 +2,63 @@ package cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.networ
 
 import java.util.List;
 
-import ch.actifsource.core.dynamic.IDynamicResourceRepository;
 import ch.actifsource.core.job.Select;
+import ch.actifsource.core.job.Update;
 import ch.actifsource.core.model.aspects.IResourceValidationAspect;
-import ch.actifsource.core.selector.typesystem.ITypeSystem;
 import ch.actifsource.core.selector.typesystem.impl.TypeSystem;
+import ch.actifsource.core.update.IModifiable;
+import ch.actifsource.core.util.LiteralUtil;
 import ch.actifsource.core.validation.ValidationContext;
 import ch.actifsource.core.validation.inconsistency.IResourceInconsistency;
 import ch.actifsource.core.validation.inconsistency.PredicateInconsistency;
+import ch.actifsource.core.validation.inconsistency.SingleStatementInconsistency;
+import ch.actifsource.core.validation.quickfix.AbstractQuickFix;
+import cleon.architecturemethods.systemarc42.metamodel.spec._05_buildingblock_view.FunctionSpace_SystemArc42_BuildingBlockView.ISystemConfigurationFunctions;
 import cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.network.FunctionSpace_Network.IHostNodeFunctions;
-import cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.security.identity.authz.deployment.role.RolePackage;
+import cleon.modelinglanguages.network.metamodel.spec.SpecPackage;
 
-public class HostNodeValidationAspect implements IResourceValidationAspect 
-{
+public class HostNodeValidationAspect implements IResourceValidationAspect {
 
 	@Override
 	public void validate(final ValidationContext context, final List<IResourceInconsistency> inconsistencyList) {
-		final ITypeSystem typeSystem = TypeSystem.create(context.getReadJobExecutor());
-		final IDynamicResourceRepository resourceRepository = typeSystem.getResourceRepository();
-		final IHostNode hostNode = resourceRepository.getResource(IHostNode.class, context.getResource());
-		final IHostNodeFunctions hostNodeFunctions = hostNode.extension(IHostNodeFunctions.class);
-		final List<IHostNode> hostNodes = hostNodeFunctions.AllHostNodes();
-		int count = 0;
-		for( final IHostNode host : hostNodes ) {
-			if( host.selectExportDNSRecord() == null || host.selectExportDNSRecord().booleanValue() ) {
+		final var typeSystem = TypeSystem.create(context.getReadJobExecutor());
+		final var resourceRepository = typeSystem.getResourceRepository();
+		final var hostNode = resourceRepository.getResource(IHostNode.class, context.getResource());
+		final var hostNodeFunctions = hostNode.extension(IHostNodeFunctions.class);
+		final var hostNodes = hostNodeFunctions.AllHostNodes();
+		var count = 0;
+		for (final IHostNode host : hostNodes) {
+			if (host.selectExportDNSRecord() == null || host.selectExportDNSRecord().booleanValue()) {
 				++count;
 			}
 		}
 		if (count > 1) {
-			final String errormessage = String.format("%1$s: to many exports (total %2$d)", Select.simpleName(context.getReadJobExecutor(), hostNode.selectHost().getResource()), count);
-			inconsistencyList.add(new PredicateInconsistency(context.getPackage(), context.getResource(), RolePackage.RoleActorResponsibility, errormessage));
+			final var errormessage = String.format("%1$s: to many exports (total %2$d)",
+					Select.simpleName(context.getReadJobExecutor(), hostNode.selectHost().getResource()), count);
+			inconsistencyList.add(new PredicateInconsistency(context.getPackage(), context.getResource(),
+					SpecPackage.AbstractPhysicalNetwork_nodes, errormessage));
 		}
-	}	
+
+		final var systemConfigurationFunction = hostNode.selectHost().selectInstanceOf()
+				.extension(ISystemConfigurationFunctions.class);
+		if (!(hostNodeFunctions.DNSRecordSet().equals(systemConfigurationFunction.AllowDNSRecordExport()))) {
+			final var errormessage = "Exporting DNS Records is not allowed. Please disable DNS record export.";
+
+			final AbstractQuickFix quickfix = new AbstractQuickFix("Set export DNS record to false", "", () -> true) {
+
+				@Override
+				protected void doApply(IModifiable modifiable) {
+					Update.createOrModifyStatement(modifiable, context.getPackage(), context.getResource(), SpecPackage.AbstractNetworkNode_exportDNSRecord, LiteralUtil.create(false));
+				}
+
+			};
+
+			final var attributeRelation = Select.statementForAttributeOrNull(context.getReadJobExecutor(),
+					SpecPackage.AbstractNetworkNode_exportDNSRecord,
+					context.getResource());
+			inconsistencyList.add(new SingleStatementInconsistency(attributeRelation, errormessage, quickfix));
+
+		}
+
+	}
 }

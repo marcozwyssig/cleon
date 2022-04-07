4887e82e8201171b7d4a787353f75946d64f3bf5
Marco Zwyssig <marco@zwyssig.info>
2022-04-07 13:12:40 +0200

    update concept

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/spec/_08_concepts/javamodel/ISystemConcepts.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/spec/_08_concepts/javamodel/ISystemConcepts.java
@@ -24,6 +24,8 @@ public interface ISystemConcepts extends cleon.architecturemethods.arc42.metamod
   
   public cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.deployment.javamodel.IDeploymentConcept selectDeploymentConcept();
   
+  public cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.update.javamodel.IUpdateConcept selectUpdateConcept();
+  
   public cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.access.javamodel.IAccessConcept selectAccessConcept();
   
   public java.util.List<? extends cleon.common.doc.metamodel.spec.chapter.javamodel.INamedChapter> selectChapters();
@@ -32,4 +34,4 @@ public interface ISystemConcepts extends cleon.architecturemethods.arc42.metamod
   
 }
 
-/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,6e1a6c6b-f190-11e8-b3e7-15e2dd8d9b05,mOLULimMgVBT6yamHCu4shvwTNA=] */
+/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,6e1a6c6b-f190-11e8-b3e7-15e2dd8d9b05,ZNKO2o31Y69I05hotpvDWtOGfjc=] */
---
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/spec/_08_concepts/update/UpdatePackage.java
@@ -0,0 +1,28 @@
+package cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.update;
+
+import ch.actifsource.core.Resource;
+
+public class UpdatePackage {
+  
+  public static final Resource UpdateConcept = new Resource("34990ceb-b65a-11ec-b559-05e0c6b23962");
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private UpdatePackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.architecturemethods.systemarc42.metamodel";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.update";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.update,0D0KXmhrHlU9fiXsF4usuaM+BgE=] */

da94c89e60770126817562e4b019470ef8c9761e
Marco Zwyssig <marco@zwyssig.info>
2022-05-17 11:54:02 +0200

    reordered the name

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/spec/_08_concepts/topology/javamodel/ISite.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/spec/_08_concepts/topology/javamodel/ISite.java
@@ -2,10 +2,10 @@ package cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topolo
 
 import ch.actifsource.util.collection.IMultiMapOrdered;
 
-public interface ISite extends cleon.common.resources.metamodel.spec.naming.javamodel.IShortName, cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractSiteWithFunctionID {
+public interface ISite extends cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.topology.javamodel.IAbstractSiteWithFunctionID, cleon.common.resources.metamodel.spec.naming.javamodel.IShortName {
 
   public static final ch.actifsource.core.INode TYPE_ID = new ch.actifsource.core.Resource("617548ea-8940-11ea-b3b4-8399156700e6");
   
 }
 
-/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,617548ea-8940-11ea-b3b4-8399156700e6,Ohx8oDyNfKzkqyqO3D8MRKexrO4=] */
+/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,617548ea-8940-11ea-b3b4-8399156700e6,7si1z6KGZpRQ7hkEJPAtLabxOrk=] */

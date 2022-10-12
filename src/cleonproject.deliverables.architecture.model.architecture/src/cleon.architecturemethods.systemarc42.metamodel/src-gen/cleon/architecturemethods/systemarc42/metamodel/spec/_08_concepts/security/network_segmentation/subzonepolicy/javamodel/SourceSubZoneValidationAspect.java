2f8fb7a11e29558baf94886e74024c74c15184b9
Marco Zwyssig <marco@zwyssig.info>
2022-10-12 13:47:53 +0200

    fixed

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/spec/_08_concepts/security/network_segmentation/subzonepolicy/javamodel/SourceSubZoneValidationAspect.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.architecturemethods.systemarc42.metamodel/src-gen/cleon/architecturemethods/systemarc42/metamodel/spec/_08_concepts/security/network_segmentation/subzonepolicy/javamodel/SourceSubZoneValidationAspect.java
@@ -1,42 +1,41 @@
 package cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.security.network_segmentation.subzonepolicy.javamodel;
 
-import ch.actifsource.core.Statement;
-import ch.actifsource.core.dynamic.IDynamicResourceRepository;
 import ch.actifsource.core.job.Select;
 import ch.actifsource.core.model.aspects.IResourceValidationAspect;
-import ch.actifsource.core.selector.typesystem.ITypeSystem;
 import ch.actifsource.core.selector.typesystem.impl.TypeSystem;
 import ch.actifsource.core.validation.ValidationContext;
 import ch.actifsource.core.validation.inconsistency.IResourceInconsistency;
+import ch.actifsource.core.validation.inconsistency.PredicateInconsistency;
 import ch.actifsource.core.validation.inconsistency.SingleStatementInconsistency;
 import ch.actifsource.util.log.Logger;
 import cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.security.network_segmentation.subzonepolicy.FunctionSpace_SubzonePolicy.ISubZoneAccessPolicyFunctions;
+import cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.security.identity.authz.deployment.activity.ActivityPackage;
 import cleon.architecturemethods.systemarc42.metamodel.spec._08_concepts.security.network_segmentation.subzonepolicy.SubzonepolicyPackage;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 
-public class SubZoneAccessPolicyValidationAspect  implements IResourceValidationAspect {
+public class SourceSubZoneValidationAspect  implements IResourceValidationAspect {
 	@Override
 	public void validate(ValidationContext validationContext, List<IResourceInconsistency> validationList) {
 		final var start = Instant.now();
 		try {
 			final var typeSystem = TypeSystem.create(validationContext.getReadJobExecutor());
 			final var resourceRepository = typeSystem.getResourceRepository();
-			final var subZoneAccessPolicy = resourceRepository.getResource(ISubZoneAccessPolicy.class, validationContext.getResource());
-			final var sourceSubZone = SourceSubZone.selectToMeDestinationSubZonePolicy(subZoneAccessPolicy);
+			final var sourceSubZone = resourceRepository.getResource(ISourceSubZone.class, validationContext.getResource());
+			
+			for( var subZoneAccessPolicy : sourceSubZone.selectDestinationSubZonePolicy().values()) {
+				final var functions = subZoneAccessPolicy.extension(ISubZoneAccessPolicyFunctions.class);
+				if( sourceSubZone.selectSourceSecuritySubZone().equals(subZoneAccessPolicy.selectPolicyForDestinationSecuritySubZone())) {
+					return;
+				}
 
-			final var functions = subZoneAccessPolicy.extension(ISubZoneAccessPolicyFunctions.class);
-			if( sourceSubZone.selectSourceSecuritySubZone().equals(subZoneAccessPolicy.selectPolicyForDestinationSecuritySubZone())) {
-				return;
+				if( !functions.HasSources() ) {
+					validationList.add(new PredicateInconsistency(validationContext.getPackage(), subZoneAccessPolicy.getResource(), SubzonepolicyPackage.SourceSubZone_destinationSubZonePolicy, String.format("no communication to this subzone")));
+				}				
 			}
 
-			if( !functions.HasSources() ) {
-
-				final var targetSubZone = Select.relationStatementOrNull(validationContext.getReadJobExecutor(), SubzonepolicyPackage.SourceSubZone_destinationSubZonePolicy, SourceSubZone.selectToMeDestinationSubZonePolicy(subZoneAccessPolicy).getResource());
-				validationList.add(new SingleStatementInconsistency(targetSubZone, String.format("no communication to this subzone")));
-			}
 		} finally {
 			final var finish = Instant.now();
 			final var timeElapsed = Duration.between(start, finish).toMillis();

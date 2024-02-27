6f28f255874bdd955e8e350a18a7e537fc491b6c
Marco Zwyssig <marco.zwyssig@ruag.ch>
2024-02-27 14:43:13 +0100

    fixed adoc and docbook generation

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.projectmethods.hermes.sample/doc/asciidoc-html-all.cmd
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.projectmethods.hermes.sample/doc/asciidoc-html-all.cmd
@@ -1,5 +1,5 @@
 chcp 65001
 cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_detailstudy/Sample_Simple_Analyse_Detailstudie.adoc
 cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_projectmanagement/Sample_Ressourcen.adoc
-cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_projectmanagement/20180312_My_Protocol/20180312_My_Protocol.adoc
+cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_projectmanagement/20180312_My_Protocol//20180312_My_Protocol.adoc
 cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_projectmanagement/Sample_Planung.adoc
--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.projectmethods.hermes.sample/doc/asciidoc-pdf-all.cmd
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.projectmethods.hermes.sample/doc/asciidoc-pdf-all.cmd
@@ -1,5 +1,5 @@
 chcp 65001
 cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_detailstudy/Sample_Simple_Analyse_Detailstudie.adoc
 cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_projectmanagement/Sample_Ressourcen.adoc
-cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_projectmanagement/20180312_My_Protocol/20180312_My_Protocol.adoc
+cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_projectmanagement/20180312_My_Protocol//20180312_My_Protocol.adoc
 cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_projectmanagement/Sample_Planung.adoc

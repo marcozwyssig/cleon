6b71e3b614fe1f7bf9d6f7ceba2b5ffa14ee4ae4
Marco Zwyssig <marco@zwyssig.info>
2024-07-26 15:05:37 +0200

    optimized cidr aspect and validation

--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.modelinglanguages.network.metamodel/src-gen/cleon/modelinglanguages/network/metamodel/spec/ipv4/javamodel/CidrAspectDecorator.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.modelinglanguages.network.metamodel/src-gen/cleon/modelinglanguages/network/metamodel/spec/ipv4/javamodel/CidrAspectDecorator.java
@@ -18,54 +18,76 @@ import cleon.modelinglanguages.network.metamodel.spec.javamodel.IAbstractPhysica
 
 public class CidrAspectDecorator extends AspectImplementationDecorator {
 
-	private static void addNetworks(IReadJobExecutor executor, IIPv4_Mask cidr, NodeSet ipNodeSet) {
-	    final int mask = cidr.selectMask();
-	    final var range = cidr.extension(IIPv4_MaskFunctions.class).SelectIPRange();
-	    if ((range == null) || (mask <= 20 && mask > 32)) {
-	        return;
-	    }
+    @Override
+    public INodeSet getDecoratableNodes(IReadJobExecutor executor, INode subject, INode decoratingRelation) {
+        final var typeSystem = TypeSystem.create(executor);
+        final var network = typeSystem.getResourceRepository().getResource(IAbstractPhysicalNetwork.class, subject);
+        final var ipNodeSet = new NodeSet();
+        for (IIPv4_Mask cidr : network.selectCidrs()) {
+            addNetworks(executor, cidr, ipNodeSet);
+        }
+        return ipNodeSet;
+    }
 
-	    try {
-	        final var subnet = new SubnetUtils(Select.simpleName(executor, cidr.getResource()));
-	        final var rangeFunctions = range.extension(IIPRangeFunctions.class);
-	        if (mask == 32) {
-	            addIPv4Address(rangeFunctions, subnet.getInfo().getAddress(), ipNodeSet);
-	        } else {
-	            for (final String ip : subnet.getInfo().getAllAddresses()) {
-	                addIPv4Address(rangeFunctions, ip, ipNodeSet);
-	            }
-	        }
-	    } catch (final IllegalArgumentException e) {
-	        logException(e);
-	    }
-	}
-	
-	private static void addIPv4Address(IIPRangeFunctions range, String ipAddress, NodeSet ipNodeSet) {
-	    final var ipv4 = range.toIPv4(ipAddress);
-	    if (ipv4 != null) {
-	        ipNodeSet.add(ipv4.getResource());
-	    }
-	}
+    private static void addNetworks(IReadJobExecutor executor, IIPv4_Mask cidr, NodeSet ipNodeSet) {
+        final int mask = cidr.selectMask();
+        final var range = cidr.extension(IIPv4_MaskFunctions.class).SelectIPRange();
+        if ((range == null) || (mask <= 20 || mask > 32)) {
+            return;
+        }
 
-	private static void logException(Exception e) {
-	    try (StringWriter out = new StringWriter();
-	        PrintWriter writer = new PrintWriter(out)) {
-	        e.printStackTrace(writer);
-	        Logger.instance().logInfo(out.toString());
-	    } catch (IOException e1) {
-			// TODO Auto-generated catch block
-	    	Logger.instance().logInfo(e1.toString());
-		}
-	}	
+        try {
+            final var network = Select.simpleName(executor, cidr.getResource());
+            if (shouldSkipValidation(network)) {
+                return;
+            }
 
-	@Override
-	public INodeSet getDecoratableNodes(IReadJobExecutor executor, INode subject, INode decoratingRelation) {
-	    final var typeSystem = TypeSystem.create(executor);
-	    final var network = typeSystem.getResourceRepository().getResource(IAbstractPhysicalNetwork.class, subject);
-	    final var ipNodeSet = new NodeSet();
-	    for (IIPv4_Mask cidr : network.selectCidrs()) {
-	        addNetworks(executor, cidr, ipNodeSet);
-	    }
-	    return ipNodeSet;
-	}
+            final var subnet = new SubnetUtils(network);
+            final var rangeFunctions = range.extension(IIPRangeFunctions.class);
+
+            if (mask == 32) {
+                addIPv4Address(rangeFunctions, subnet.getInfo().getAddress(), ipNodeSet);
+            } else {
+                for (final String ip : subnet.getInfo().getAllAddresses()) {
+                    addIPv4Address(rangeFunctions, ip, ipNodeSet);
+                }
+            }
+        } catch (final IllegalArgumentException e) {
+            logException(e);
+        }
+    }
+
+    private static boolean shouldSkipValidation(final String network) {
+        final int mask = getMaskFromNetwork(network);
+        if (mask < 24 || mask > 32) {
+            Logger.instance().logInfo(String.format("Validation skipped (%s)", network));
+            return true;
+        }
+        return false;
+    }
+
+    private static int getMaskFromNetwork(final String network) {
+        try {
+            return Integer.parseInt(network.split("/")[1]);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            logException(e);
+            return -1; // Invalid mask value, skipping validation
+        }
+    }
+
+    private static void addIPv4Address(IIPRangeFunctions range, String ipAddress, NodeSet ipNodeSet) {
+        final var ipv4 = range.toIPv4(ipAddress);
+        if (ipv4 != null) {
+            ipNodeSet.add(ipv4.getResource());
+        }
+    }
+
+    private static void logException(Exception e) {
+        try (StringWriter out = new StringWriter(); PrintWriter writer = new PrintWriter(out)) {
+            e.printStackTrace(writer);
+            Logger.instance().logInfo(out.toString());
+        } catch (IOException e1) {
+            Logger.instance().logInfo(e1.toString());
+        }
+    }
 }
--- src/cleonproject.deliverables.architecture.model.architecture/src/cleon.modelinglanguages.network.metamodel/src-gen/cleon/modelinglanguages/network/metamodel/spec/ipv4/javamodel/CidrValidationAspect.java
+++ src/cleonproject.deliverables.architecture.model.architecture/src/cleon.modelinglanguages.network.metamodel/src-gen/cleon/modelinglanguages/network/metamodel/spec/ipv4/javamodel/CidrValidationAspect.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 
+import ch.actifsource.core.Statement;
 import ch.actifsource.core.job.Select;
 import ch.actifsource.core.model.aspects.IResourceValidationAspect;
 import ch.actifsource.core.selector.typesystem.impl.TypeSystem;
@@ -18,6 +19,8 @@ import cleon.modelinglanguages.network.metamodel.spec.javamodel.IAbstractPhysica
 
 import java.util.stream.Stream;
 
+import javax.annotation.CheckForNull;
+
 public class CidrValidationAspect implements IResourceValidationAspect {
 
 	@Override
@@ -26,7 +29,8 @@ public class CidrValidationAspect implements IResourceValidationAspect {
 		final var resourceRepository = typeSystem.getResourceRepository();
 		final var cidr = resourceRepository.getResource(IIPv4_Mask.class, validationContext.getResource());
 		final var networkObj = IPv4_Mask_Aware.selectToMeCidrs(cidr);
-		if( !(networkObj instanceof IAbstractPhysicalNetwork)) {
+		
+		if (!(networkObj instanceof IAbstractPhysicalNetwork)) {
 			return;
 		}
 
@@ -34,45 +38,61 @@ public class CidrValidationAspect implements IResourceValidationAspect {
 		final var network = Select.simpleName(validationContext.getReadJobExecutor(), cidr.getResource());
 
 		try {
-			final var range = cidr.extension(IIPv4_MaskFunctions.class).SelectIPRange();
-			final var subnet = new SubnetUtils(network);
-			final var cidrStatement = Select.relationStatementOrNull(validationContext.getReadJobExecutor(), Ipv4Package.IPv4_aE_Mask_aE_Aware_cidrs, IPv4_Mask_Aware.selectToMeCidrs(cidr).getResource());
-			
-			final var subNetInfo = subnet.getInfo();
-			final var calculatedNetworkAddress = subNetInfo.getNetworkAddress();
-
-	        // Check if the provided CIDR notation correctly matches the calculated network range
-			final var isCidrNotationCorrect = network.split("/")[0].equals(calculatedNetworkAddress);
-	        if( !isCidrNotationCorrect && cidr.selectMask() != 32) {
-		        validationList.add(new SingleStatementInconsistency(cidrStatement, String.format("Invalid CIDR " + network + ". Expected Network Address: " + calculatedNetworkAddress, cidrStatement)));
-		        return;
-	        }
-	        
-			// Performance with validation of extensive networks		
-			if (cidr.selectMask() < 24) {
-			    Logger.instance().logInfo(String.format("Validation skipped (%s)", network));
-			    return;
+			if (shouldSkipValidation(network)) {
+				return;
 			}
-	        
-			final var allAddresses = subNetInfo.getAllAddresses();
-
-			Stream.of(allAddresses).forEach(ip -> {
-			    final var ipv4 = range.extension(IIPRangeFunctions.class).toIPv4(ip);
-			    if (ipv4 == null) {
-			        // Add final quick fix
-			        final var fixMissingIP = new FixMissingIPQuickfix(subnet, range, () -> true);
-			        validationList.add(new SingleStatementInconsistency(cidrStatement, String.format("IP address %s not in IP range", ip), fixMissingIP));
-			    }
-			});
+			validateCidr(validationContext, cidr, network, validationList);
+		} finally {
+			logValidationTime(start);
 		}
-		finally {
-			final var finish = Instant.now();
-			final var timeElapsed = Duration.between(start, finish).toMillis();
-			if (timeElapsed > 100) {
-			    Logger.instance().logInfo(String.format("Validation time for %s took %d ms", this.getClass().getSimpleName(), timeElapsed));
-			}			
+	}
+	
+	private static boolean shouldSkipValidation(final String network) {
+		final int mask = Integer.parseInt(network.split("/")[1]);
+		if (mask < 24) {
+			Logger.instance().logInfo(String.format("Validation skipped (%s)", network));
+			return true;
+		}
+		return false;
+	}
+	
+
+	private static void validateCidr(final ValidationContext validationContext, final IIPv4_Mask cidr, final String network, final List<IResourceInconsistency> validationList) {
+		final var range = cidr.extension(IIPv4_MaskFunctions.class).SelectIPRange();
+		final var subnet = new SubnetUtils(network);
+		final var cidrStatement = Select.relationStatementOrNull(validationContext.getReadJobExecutor(), Ipv4Package.IPv4_aE_Mask_aE_Aware_cidrs, IPv4_Mask_Aware.selectToMeCidrs(cidr).getResource());
+		final var subNetInfo = subnet.getInfo();
+		final var calculatedNetworkAddress = subNetInfo.getNetworkAddress();
+
+		if (!isCidrNotationCorrect(network, calculatedNetworkAddress, cidr)) {
+			validationList.add(new SingleStatementInconsistency(cidrStatement, String.format("Invalid CIDR %s. Expected Network Address: %s", network, calculatedNetworkAddress, cidrStatement)));
+			return;
 		}
 
+		validateIpAddresses(range, subNetInfo, subnet, cidrStatement, validationList);
+	}
 
+	private static boolean isCidrNotationCorrect(final String network, final String calculatedNetworkAddress, final IIPv4_Mask cidr) {
+		return network.split("/")[0].equals(calculatedNetworkAddress) || cidr.selectMask() == 32;
+	}
+
+	private static  void validateIpAddresses(final IIPRange range, final SubnetUtils.SubnetInfo subNetInfo, final SubnetUtils subnet, final @CheckForNull Statement cidrStatement, final List<IResourceInconsistency> validationList) {
+		final var allAddresses = subNetInfo.getAllAddresses();
+
+		Stream.of(allAddresses).forEach(ip -> {
+			final var ipv4 = range.extension(IIPRangeFunctions.class).toIPv4(ip);
+			if (ipv4 == null) {
+				final var fixMissingIP = new FixMissingIPQuickfix(subnet, range, () -> true);
+				validationList.add(new SingleStatementInconsistency(cidrStatement, String.format("IP address %s not in IP range", ip), fixMissingIP));
+			}
+		});
+	}
+
+	private void logValidationTime(final Instant start) {
+		final var finish = Instant.now();
+		final var timeElapsed = Duration.between(start, finish).toMillis();
+		if (timeElapsed > 100) {
+			Logger.instance().logInfo(String.format("Validation time for %s took %d ms", this.getClass().getSimpleName(), timeElapsed));
+		}
 	}
 }

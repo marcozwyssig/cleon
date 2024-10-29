2b36dbd0b94c0fbc46d258fa07fccc18e6ffda79
Marco Zwyssig <marco.zwyssig@ruag.ch>
2024-10-29 17:43:25 +0100

    migrated cleon to root

--- src/cleonproject.deliverables.architecture.model.architecture/doc/asciidoc-pdf-all.cmd
+++
@@ -1,2 +0,0 @@
-chcp 65001
-cmd /c bundle exec asciidoctor-pdf cleonproject_deliverables_architecture_model_architecture/Cleon_Architecture.adoc
--- src/cleonproject.site/build.cmd
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-SET var=file:/%cd%
-ECHO Repository: %var%
-
-eclipsec -application -nosplash org.eclipse.equinox.p2.publisher.UpdateSitePublisher -metadataRepository %var% -artifactRepository %var% -source ./ --compress publishArtifacts

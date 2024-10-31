538b0ba14e115d83573fe737015c3b50b22debb9
Marco Zwyssig <marco@zwyssig.info>
2024-10-31 11:39:32 +0100

    moved

---
+++ doc/Cleon-html.cmd
@@ -0,0 +1,2 @@
+chcp 65001
+cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri Cleon.adoc 
---
+++ doc/asciidoc-html-all.cmd
@@ -0,0 +1,2 @@
+chcp 65001
+cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleonproject_deliverables_architecture_model_architecture/Cleon_Architecture.adoc

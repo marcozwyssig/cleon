6db6e9a6168838f559b01bb6192a3a53ffdf1758
Marco Zwyssig <marco.zwyssig@ruag.ch>
2024-11-18 13:27:17 +0100

    complete full generate

--- src/cleon.architecturemethods.arc42.sample/doc/asciidoc-pdf-all.cmd
+++ src/cleon.architecturemethods.arc42.sample/doc/asciidoc-pdf-all.cmd
@@ -1,3 +1,3 @@
 chcp 65001
-cmd /c bundle exec asciidoctor-pdf cleon_architecturemethods_arc42_sample_arc42/de/Chess_Architecture.adoc
-cmd /c bundle exec asciidoctor-pdf cleon_architecturemethods_arc42_sample_software/de/SampleArchitecture_Architecture.adoc
+cmd /c bundle exec asciidoctor-pdf cleon_architecturemethods_arc42_sample_arc42/de/Chess_Systemarchitektur.adoc
+cmd /c bundle exec asciidoctor-pdf cleon_architecturemethods_arc42_sample_software/de/SampleArchitecture_Systemarchitektur.adoc

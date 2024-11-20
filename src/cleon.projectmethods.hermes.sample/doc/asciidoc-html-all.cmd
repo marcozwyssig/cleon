f972cb4ec24849413e47419bb6be3e893aa204f9
Marco Zwyssig <marco.zwyssig@ruag.ch>
2024-11-20 15:29:27 +0100

    enhanced protocol model

--- src/cleon.projectmethods.hermes.sample/doc/asciidoc-html-all.cmd
+++ src/cleon.projectmethods.hermes.sample/doc/asciidoc-html-all.cmd
@@ -1,5 +1,6 @@
 chcp 65001
 cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_detailstudy/de/Sample_Simple_Analyse.adoc
 cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_projectmanagement/de/Sample.adoc
-cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_projectmanagement/de/My_Protocol.adoc
+cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_projectmanagement/de/Mein_Protokoll.adoc
+cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_projectmanagement/en/My_Protocol.adoc
 cmd /c bundle exec asciidoctor -r asciidoctor-diagram -a data-uri cleon_projectmethods_hermes_sample_projectmanagement/de/Sample_Planung.adoc
--- src/cleon.projectmethods.hermes.sample/doc/asciidoc-pdf-all.cmd
+++ src/cleon.projectmethods.hermes.sample/doc/asciidoc-pdf-all.cmd
@@ -1,5 +1,6 @@
 chcp 65001
 cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_detailstudy/de/Sample_Simple_Analyse.adoc
 cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_projectmanagement/de/Sample.adoc
-cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_projectmanagement/de/My_Protocol.adoc
+cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_projectmanagement/de/Mein_Protokoll.adoc
+cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_projectmanagement/en/My_Protocol.adoc
 cmd /c bundle exec asciidoctor-pdf cleon_projectmethods_hermes_sample_projectmanagement/de/Sample_Planung.adoc

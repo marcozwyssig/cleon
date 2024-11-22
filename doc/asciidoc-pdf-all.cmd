70531e747c562d68dc2052ca06fc50835a3c72b9
Marco Zwyssig <marco@zwyssig.info>
2024-11-22 09:32:58 +0100

    regenerated

--- doc/asciidoc-pdf-all.cmd
+++ doc/asciidoc-pdf-all.cmd
@@ -1,2 +1,2 @@
 chcp 65001
-cmd /c bundle exec asciidoctor-pdf cleonproject_deliverables_architecture_model_architecture/de/Cleon_Architecture.adoc
+cmd /c bundle exec asciidoctor-pdf cleonproject_deliverables_architecture_model_architecture/de/Cleon_Systemarchitektur.adoc
---
+++ doc/cleonproject_deliverables_architecture_model_architecture/de/Cleon_Systemarchitektur-confluence-create.cmd
@@ -0,0 +1,2 @@
+chcp 65001
+cmd /c bundle exec asciidoctor-confluence --host %CONFLUENCE_HOST% --spaceKey %CONFLUENCE_SPACE_KEY% --title Cleon_Systemarchitektur --username %CONFLUENCE_USER_NAME% --password %CONFLUENCE_PASSWORD% Cleon_Systemarchitektur.adoc

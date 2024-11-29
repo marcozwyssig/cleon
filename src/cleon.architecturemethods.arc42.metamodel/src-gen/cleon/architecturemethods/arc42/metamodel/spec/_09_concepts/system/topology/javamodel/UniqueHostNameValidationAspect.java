26e335a5e3c2e638ede7c159b55290d4f05d5de6
Marco Zwyssig <marco@zwyssig.info>
2024-11-29 14:53:09 +0100

    no lazy loading

--- src/cleon.architecturemethods.arc42.metamodel/src-gen/cleon/architecturemethods/arc42/metamodel/spec/_09_concepts/system/topology/javamodel/UniqueHostNameValidationAspect.java
+++ src/cleon.architecturemethods.arc42.metamodel/src-gen/cleon/architecturemethods/arc42/metamodel/spec/_09_concepts/system/topology/javamodel/UniqueHostNameValidationAspect.java
@@ -2,8 +2,9 @@ package cleon.architecturemethods.arc42.metamodel.spec._09_concepts.system.topol
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 import ch.actifsource.core.job.Select;
 import ch.actifsource.core.model.aspects.IResourceValidationAspect;
@@ -16,37 +17,50 @@ import cleon.common.resources.metamodel.spec.id.IdPackage;
 
 public class UniqueHostNameValidationAspect implements IResourceValidationAspect {
 
-	private static String getHostname( final IAbstractHost abstractHost ) {
-		return Select.simpleName(abstractHost.getReadJobExecutor(), abstractHost.getResource());
-	}
-	
-	@Override
-	public void validate(final ValidationContext context, final List<IResourceInconsistency> inconsistencyList) {
-		final var start = Instant.now();
-		try {
-			final var typeSystem = context.getTypeSystem();
-			final var resourceRepository = typeSystem.getResourceRepository();
-			final var abstractHost = resourceRepository.getResource(IAbstractHost.class, context.getResource());
-			final var abstractHostFunctions = abstractHost.extension(IAbstractHostFunctions.class);
-			final var allHosts = abstractHostFunctions.AllHosts();
-			
-			final var duplicateItems = allHosts.stream()
-					.filter(x -> getHostname(x).equals(getHostname(abstractHost)))
-					.collect(Collectors.toList());
-			
-			if (!duplicateItems.isEmpty() && duplicateItems.size() != 1) {
-				final var errormessage = String.format("Hostname is not unique.");
-				inconsistencyList.add(new PredicateInconsistency(context.getPackage(), context.getResource(),
-						IdPackage.IntegerBusinessObjectId_identifier, errormessage));
-			}
-		} finally {
-			final var finish = Instant.now();
-			final var timeElapsed = Duration.between(start, finish).toMillis();
-			if( timeElapsed > 100 ) {
-				Logger.instance().logInfo(String.format("Validation time for %s took %d ms", this.getClass().getSimpleName(), timeElapsed));
-			}
+    private static String getHostname(final IAbstractHost abstractHost) {
+        return Select.simpleName(abstractHost.getReadJobExecutor(), abstractHost.getResource());
+    }
 
-		}
+    @Override
+    public void validate(final ValidationContext context, final List<IResourceInconsistency> inconsistencyList) {
+        final var start = Instant.now();
+        try {
+            final var typeSystem = context.getTypeSystem();
+            final var resourceRepository = typeSystem.getResourceRepository();
+            final var abstractHost = resourceRepository.getResource(IAbstractHost.class, context.getResource());
+            final var abstractHostFunctions = abstractHost.extension(IAbstractHostFunctions.class);
+            final var allHosts = abstractHostFunctions.AllHosts();
 
-	}
+            // Cache the current host's hostname
+            final var currentHostname = getHostname(abstractHost);
+
+            // Use a set to track duplicates for faster validation
+            final Set<String> seenHostnames = new HashSet<>();
+            boolean isDuplicate = false;
+
+            for (var host : allHosts) {
+                final var hostname = getHostname(host);
+
+                // Check if hostname matches the current hostname
+                if (hostname.equals(currentHostname)) {
+                    if (!seenHostnames.add(hostname)) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+            }
+
+            if (isDuplicate) {
+                final var errorMessage = "Hostname is not unique.";
+                inconsistencyList.add(new PredicateInconsistency(context.getPackage(), context.getResource(),
+                        IdPackage.IntegerBusinessObjectId_identifier, errorMessage));
+            }
+        } finally {
+            final var finish = Instant.now();
+            final var timeElapsed = Duration.between(start, finish).toMillis();
+            if (timeElapsed > 100) {
+                Logger.instance().logInfo(String.format("Validation time for %s took %d ms", this.getClass().getSimpleName(), timeElapsed));
+            }
+        }
+    }
 }

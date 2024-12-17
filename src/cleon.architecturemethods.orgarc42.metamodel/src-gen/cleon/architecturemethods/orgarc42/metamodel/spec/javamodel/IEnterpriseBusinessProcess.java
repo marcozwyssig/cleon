f831dffe79b13bf077637c74a20721b1c9729006
Marco Zwyssig <marco.zwyssig@ruag.ch>
2024-12-17 16:29:55 +0100

    updated

--- src/cleon.architecturemethods.orgarc42.metamodel/src-gen/cleon/architecturemethods/orgarc42/metamodel/spec/javamodel/IEnterpriseBusinessProcess.java
+++ src/cleon.architecturemethods.orgarc42.metamodel/src-gen/cleon/architecturemethods/orgarc42/metamodel/spec/javamodel/IEnterpriseBusinessProcess.java
@@ -2,7 +2,7 @@ package cleon.architecturemethods.orgarc42.metamodel.spec.javamodel;
 
 import ch.actifsource.util.collection.IMultiMapOrdered;
 
-public interface IEnterpriseBusinessProcess extends cleon.modelinglanguages.bpmn.metamodel.spec.javamodel.IBpmn, ch.actifsource.core.javamodel.INamedResource {
+public interface IEnterpriseBusinessProcess extends ch.actifsource.core.javamodel.INamedResource, cleon.modelinglanguages.bpmn.metamodel.spec.javamodel.IBpmn {
 
   public static final ch.actifsource.core.INode TYPE_ID = new ch.actifsource.core.Resource("037d0836-bb6a-11e3-bf59-6b10d9dd6d36");
   
@@ -12,4 +12,4 @@ public interface IEnterpriseBusinessProcess extends cleon.modelinglanguages.bpmn
   
 }
 
-/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,037d0836-bb6a-11e3-bf59-6b10d9dd6d36,N6w5coARMs/UtuARritOw5DJ2Vc=] */
+/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,037d0836-bb6a-11e3-bf59-6b10d9dd6d36,VBUTO7/fu/tg6le/ezKpR3UaOXk=] */

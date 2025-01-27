5cfe3bb28644e5e25e25d5601fc7c9cc591a3bf9
Marco Zwyssig <marco.zwyssig@ruag.ch>
2025-01-27 15:52:27 +0100

    moved chocolatey into operationsmethods

---
+++ src/cleon.operationalmethods.chocolatey.metamodel/src-gen/cleon/operationalmethods/chocolatey/metamodel/template/nuget/NugetPackage.java
@@ -0,0 +1,26 @@
+package cleon.operationalmethods.chocolatey.metamodel.template.nuget;
+
+import ch.actifsource.core.Resource;
+
+public class NugetPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private NugetPackage() {}
+  
+  public static final String getScopeName() {
+    return "cleon.operationalmethods.chocolatey.metamodel";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "cleon.operationalmethods.chocolatey.metamodel.template.nuget";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,cleon.operationalmethods.chocolatey.metamodel.template.nuget,jx2M4zWSlrcWIj+j0RtM8FBEg3Y=] */
